81a1abff61947883b0cd07f820f2a50cde9af627
Konlan Mark Baba <mrhexa123@proton.me>
2023-02-17 17:23:18 -0800

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @void: Null value
+ *
+ * Description: Print small letters of alphabet
+ * Return: Zero value to succesful
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	};
+	return (0);
+}
