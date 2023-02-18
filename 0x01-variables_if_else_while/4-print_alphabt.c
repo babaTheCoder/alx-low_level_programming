c93d5c571303b65efd630d3a2f1bd3b691959991
Konlan Mark Baba <mrhexa123@proton.me>
2023-02-17 17:46:39 -0800

    completed task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * @void: Null value
+ *
+ * Description: Print alphabet lowercase
+ * Return: Zero value
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'q' || ch == 'e')
+			continue;
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
