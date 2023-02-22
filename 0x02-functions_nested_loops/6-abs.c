b568061558710c308e7587a3b528a415dd970cd8
Konlan Mark Baba <mrhexa123@proton.me>
2023-02-21 19:42:01 -0800

    no number is absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _abs - calculate the absolute value of the number from zero.
+ * @c: the integer to be converted.
+ * Return: absolute value of the input number.
+ */
+
+
+int _abs(int c)
+{
+	if (c < 0)
+	{
+		return (-c);
+	}
+	else if (c > 0)
+	{
+		return (c);
+	}
+	else
+	{
+		return (c);
+	}
+}
