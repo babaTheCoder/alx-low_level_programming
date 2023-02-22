d71ea10f0c9d8667eb96512ee5dfce49405e7d6e
Konlan Mark Baba <mrhexa123@proton.me>
2023-02-21 18:37:26 -0800

    is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *_isalpha: checks if input is an alphabet
- * @c: character to test
- *
- * Return: 1 if alpha, 0 if not
+ * _isalpha - tests whether a character is from the English alphabet.
+ * @c: character to be checked.
+ * Return: 1 if the character is an English character.
+ * 0 if the character is not an English character.
  */
 
 int _isalpha(int c)
