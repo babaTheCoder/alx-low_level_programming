7480dc06776f6ac7be0f667f46f145f668faf5c6
Konlan Mark Baba <mrhexa123@proton.me>
2023-02-25 17:09:07 -0800

    6 - 10

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 0;
+
+	while (a <= 100)
+	{
+		if (a % 3 == 0 && a % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else 
+		{
+			printf("%d ", a);
+		}
+		a++;
+	}
+	return (0);
+}
