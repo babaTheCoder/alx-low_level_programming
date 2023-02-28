f79e27b6f1375fff8e89b93293aea644db407fbc
Konlan Mark Baba <mrhexa123@proton.me>
2023-02-28 08:01:46 -0800

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ * reset_to_98 - updates the value to 98
+ *
+ * @*n: name of pointer
+ *
+ * Returns: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+C Arrays | C Strings | C Pointers
