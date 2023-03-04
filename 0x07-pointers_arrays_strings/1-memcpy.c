0c5d71d4bc64fb303f0732894c223c071dcd54df
Konlan Mark Baba <mrhexa123@proton.me>
2023-03-04 08:33:51 -0800

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memcpy - function copies n bytes from memory area src to memory area dest
+ *
+ * @dest: destination of copied bytes
+ * @src: source of copied bytes
+ * @n: number of bytes to copy
+ *
+ * Return: pointer of type char
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	if (n <= 0)
+	{
+		return (dest);
+	}
+
+	while (i < n)
+	{
+		*(dest + i) = *(src + i);
+
+		i++;
+	}
+
+	return (dest);
+
+}
