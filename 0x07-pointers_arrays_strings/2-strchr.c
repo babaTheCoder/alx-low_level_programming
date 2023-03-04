3850c8bb02862ab8f741ac580967a0cd7ca526c3
Konlan Mark Baba <mrhexa123@proton.me>
2023-03-04 09:29:09 -0800

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr - checks for the first occurence of a char in a string
+ *
+ * @str: string to search
+ * @c: character to search
+ *
+ * Return: pointer to char c or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int lenstr = 0;
+	int i = 0;
+
+	while (s[lenstr] != '\0')
+	{
+		lenstr++;
+	}
+
+	for (i = 0; i <= lenstr; i++)
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+	}
+
+	return (NULL);
+}
