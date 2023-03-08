701f505d4e943efe9fe5d98fa4b667c33aac69da
Konlan Mark Baba <mrhexa123@proton.me>
2023-03-08 06:59:29 -0800

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - returns length of a string
+ *
+ * @s : strint to return length of
+ *
+ * Returns : length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
+}
