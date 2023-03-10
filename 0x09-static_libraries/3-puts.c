a789d1976e445017e86ceea6de4dbf0545505186
Konlan Mark Baba <mrhexa123@proton.me>
2023-03-10 14:42:01 -0800

    task 0: Static Libraries

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,5 +1,19 @@
 #include "main.h"
+#include <stdio.h>
 
-void _puts(char *s)
+/**
+ * _puts - a function that writes a string, followed by a new line to stdou
+ *
+ * @str : string to print
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
 {
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
 }
