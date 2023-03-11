55066f7dac92d3a79ccc7566f2b835cc15e1d64d
Konlan Mark Baba <mrhexa123@proton.me>
2023-03-10 17:00:22 -0800

    task 1: argc & argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints arguments passed
+ *
+ * @argc: size of argv
+ * @argv: array of commandline arguments
+ *
+ * Return: 0 Always (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) *argv;
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
