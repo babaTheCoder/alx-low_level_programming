571f6717305877ae46265265e838d4365e00d5a7
Konlan Mark Baba <mrhexa123@proton.me>
2023-03-10 16:51:33 -0800

    task 0: argc & argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,12 +9,12 @@
  * Return: 0 always (success)
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	if (argc > 0)
 	{
-		printf ("%s\n", argv[0]);
+		printf("%s\n", argv[0]);
 	}
 
-	return 0;
+	return (0);
 }
