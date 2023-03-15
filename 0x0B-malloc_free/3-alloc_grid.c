e7a1e28a48366dab1c40205813b0604249602262
Konlan Mark Baba <mrhexa123@proton.me>
2023-03-14 19:09:12 -0700

    task 3 or dynamic mem allocation with malloc()

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * alloc_grid - creates pointer to 2D array
+ *
+ * @width: width of arry
+ * @height: height of array
+ *
+ * Return: pointer to array
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i;
+	int j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	grid = (int **)malloc(height * sizeof(int *));
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int *)malloc(width * sizeof(int));
+
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+
+			free(grid);
+			return (NULL);
+		}
+
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
+}
