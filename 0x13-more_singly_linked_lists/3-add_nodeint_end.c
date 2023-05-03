a5922fb9a5a2306974d6fc39c3fd5983fe0b4208
babaTheCoder <mark_coder123@proton.me>
2023-05-03 01:29:16 -0700

    fixed betty errors in task3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -19,7 +19,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	if (endNode == 0)
 	{
-		return(0);
+		return (0);
 	}
 
 	activeNode = *head;
@@ -43,5 +43,5 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 
 
-	return(endNode);
+	return (endNode);
 }
