4d4e6b56404a0886ca104d40d61a43e32f7792bf
babaTheCoder <mark_coder123@proton.me>
2023-05-03 14:33:48 -0700

    levelling up fast

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -13,31 +13,32 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node = malloc(sizeof(list_t));
+	list_t *new_node;
+	list_t *temp = *head;
+	unsigned int len = 0;
 
+	while (str[len])
+		len++;
 
-	if (new_node == NULL)
-	{
+	new_node = malloc(sizeof(list_t));
+
+	if (!new_node)
 		return (NULL);
-	}
 
 	new_node->str = strdup(str);
+	new_node->len = len;
+	new_node->next = NULL;
 
 	if (*head == NULL)
 	{
 		*head = new_node;
+		return (new_node);
 	}
-	else
-	{
-		list_t *current_node = *head;
 
-		while (current_node->next != NULL)
-		{
-			current_node = current_node->next;
-		}
-		current_node->next = new_node;
+	while (temp->next)
+		temp = temp->next;
 
-	}
+	temp->next = new_node;
 
 	return (new_node);
 }
